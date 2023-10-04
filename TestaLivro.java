c25812b26b928d43d8ba2cddaac11db84b834576
Pedro <107764107+pedrocsf@users.noreply.github.com>
2023-10-04 12:31:53 -0300

    Update TestaLivro.java

--- TestaLivro.java
+++ TestaLivro.java
@@ -2,26 +2,40 @@ public class TestaLivro {
     public static void main(String[] args) {
         
         Livro livro1 = new Livro();
-            livro1.setTitulo("Pré-Escrita - Coleção Aprenda em Casa");
-            livro1.setAutor("Desconhecido");
-            livro1.setAno(2015);
-            livro1.setEditora("Todolivro");
+        livro1.setTitulo("Pré-Escrita - Coleção Aprenda em Casa");
+        livro1.setAutor("Desconhecido");
+        livro1.setAno(2015);
+        livro1.setEditora("Todolivro");
         
         Livro livro2 = new Livro();
-            livro2.setTitulo("Aprenda em Casa Caligrafia: Coordenação Motora");
-            livro2.setAutor("Desconhecido");
-            livro2.setAno(2021);
-            livro2.setEditora("Todolivro");
+        livro2.setTitulo("Aprenda em Casa Caligrafia: Coordenação Motora");
+        livro2.setAutor("Desconhecido");
+        livro2.setAno(2021);
+        livro2.setEditora("Todolivro");
         
         Livro livro3 = new Livro();
-            livro3.setTitulo("Perigoso!");
-            livro3.setAutor("Tim Warnes");
-            livro3.setAno(2014);
-            livro3.setEditora("Ciranda Cultural");
+        livro3.setTitulo("Perigoso!");
+        livro3.setAutor("Tim Warnes");
+        livro3.setAno(2014);
+        livro3.setEditora("Ciranda Cultural");
 
-        System.out.println("Livro 1:\n" + livro1.toString() + "\n");
-        System.out.println("Livro 2:\n" + livro2.toString() + "\n");
-        System.out.println("Livro 3:\n" + livro3.toString() + "\n");
+        System.out.println("Livro 1:");
+        System.out.println("Título: " + livro1.getTitulo());
+        System.out.println("Autor: " + livro1.getAutor());
+        System.out.println("Ano: " + livro1.getAno());
+        System.out.println("Editora: " + livro1.getEditora() + "\n");
+
+        System.out.println("Livro 2:");
+        System.out.println("Título: " + livro2.getTitulo());
+        System.out.println("Autor: " + livro2.getAutor());
+        System.out.println("Ano: " + livro2.getAno());
+        System.out.println("Editora: " + livro2.getEditora() + "\n");
+
+        System.out.println("Livro 3:");
+        System.out.println("Título: " + livro3.getTitulo());
+        System.out.println("Autor: " + livro3.getAutor());
+        System.out.println("Ano: " + livro3.getAno());
+        System.out.println("Editora: " + livro3.getEditora() + "\n");
     }
 }
 
@@ -30,6 +44,7 @@ class Livro {
     private String autor;
     private int ano;
     private String editora;
+
 
 
 
@@ -67,13 +82,5 @@ class Livro {
 
     public String getEditora() {
         return editora;
-    }
-
-
-
-
-
-    public String toString() {
-        return "Livro: " + titulo + "\nAutor: " + autor + "\nAno: " + ano + "\nEditora: " + editora;
     }
 }

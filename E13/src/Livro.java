6b86b37b3a200533e7a73354db4a3f4f6050d6bd
Pedro <107764107+pedrocsf@users.noreply.github.com>
2023-10-04 10:39:04 -0300

    Add files via upload

---
+++ E13/src/Livro.java
@@ -0,0 +1,49 @@
+public class Livro {
+    private String titulo;
+    private String autor;
+    private int ano;
+    private String editora;
+
+    public Livro(String titulo, String autor, int ano, String editora) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+        this.editora = editora;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public String toString() {
+        return "Livro: " + titulo + "\nAutor: " + autor + "\nAno: " + ano + "\nEditora: " + editora;
+    }
+}

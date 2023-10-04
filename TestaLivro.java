eb65279a7f7ff024b967de5113cdd4d6637a37bf
Pedro <107764107+pedrocsf@users.noreply.github.com>
2023-10-04 12:19:36 -0300

    Update TestaLivro.java

--- TestaLivro.java
+++ TestaLivro.java
@@ -1,9 +1,23 @@
 public class TestaLivro {
     public static void main(String[] args) {
         
-        Livro livro1 = new Livro("Pré-Escrita - Coleção Aprenda em Casa", "Desconhecido", 2015, "Todolivro");
-        Livro livro2 = new Livro("Aprenda em Casa Caligrafia: Coordenação Motora", "Desconhecido", 2021, "Todolivro");
-        Livro livro3 = new Livro("Perigoso!","Tim Warnes", 2014, "Ciranda Cultural");
+        Livro livro1 = new Livro();
+            livro1.setTitulo("Pré-Escrita - Coleção Aprenda em Casa");
+            livro1.setAutor("Desconhecido");
+            livro1.setAno(2015);
+            livro1.setEditora("Todolivro");
+        
+        Livro livro2 = new Livro();
+            livro2.setTitulo("Aprenda em Casa Caligrafia: Coordenação Motora");
+            livro2.setAutor("Desconhecido");
+            livro2.setAno(2021);
+            livro2.setEditora("Todolivro");
+        
+        Livro livro3 = new Livro();
+            livro3.setTitulo("Perigoso!");
+            livro3.setAutor("Tim Warnes");
+            livro3.setAno(2014);
+            livro3.setEditora("Ciranda Cultural");
 
         System.out.println("Livro 1:\n" + livro1.toString() + "\n");
         System.out.println("Livro 2:\n" + livro2.toString() + "\n");
@@ -17,44 +31,47 @@ class Livro {
     private int ano;
     private String editora;
 
-    public Livro(String titulo, String autor, int ano, String editora) {
-        this.titulo = titulo;
-        this.autor = autor;
-        this.ano = ano;
-        this.editora = editora;
-    }
 
-    public String getTitulo() {
-        return titulo;
-    }
+
 
     public void setTitulo(String titulo) {
         this.titulo = titulo;
-    }
-
-    public String getAutor() {
-        return autor;
     }
 
     public void setAutor(String autor) {
         this.autor = autor;
     }
 
-    public int getAno() {
-        return ano;
-    }
-
     public void setAno(int ano) {
         this.ano = ano;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAno() {
+        return ano;
     }
 
     public String getEditora() {
         return editora;
     }
 
-    public void setEditora(String editora) {
-        this.editora = editora;
-    }
+
+
+
 
     public String toString() {
         return "Livro: " + titulo + "\nAutor: " + autor + "\nAno: " + ano + "\nEditora: " + editora;
